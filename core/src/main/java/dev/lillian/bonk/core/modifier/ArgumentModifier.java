ddb650cde42f7f0bf13344a9d02911358fe8c8cb
Lillian Armes <lilyarmes@protonmail.com>
2021-07-14 10:49:42 -0400

    Fix modifiers, Attach sources on package

--- core/src/main/java/dev/lillian/bonk/core/modifier/ArgumentModifier.java
+++ core/src/main/java/dev/lillian/bonk/core/modifier/ArgumentModifier.java
@@ -5,8 +5,7 @@ import dev.lillian.bonk.core.parametric.CommandParameter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
-
 public interface ArgumentModifier<T> {
-    Optional<T> modify(@NotNull CommandExecution execution, @NotNull CommandParameter commandParameter, @Nullable T argument) throws IllegalArgumentException;
+    @Nullable
+    T modify(@NotNull CommandExecution execution, @NotNull CommandParameter commandParameter, @Nullable T argument) throws IllegalArgumentException;
 }
